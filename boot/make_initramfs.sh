1d35cd0948be40ff5d60b3c78a1ff15b59a62814
Patrick Günthard <me@patrickguenthard.ch>
2023-12-20 11:14:17 +0100

    /etc/

--- boot/make_initramfs.sh
+++ boot/make_initramfs.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 rm init.cpio
+cp -r ../src/root/etc ./initramfs/
+mkdir -p ./initramfs/dev
+mkdir -p ./initramfs/proc
+mkdir -p ./initramfs/home/patrick/
 cp -r initramfs/ initramfs_build
 cd initramfs_build/
+
 
 sudo chown -R 0:0 .
 sudo chown -R 1000:97 ./home/patrick/
